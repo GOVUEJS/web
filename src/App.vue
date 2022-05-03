85aa589c7a7c8e43ac5d349621fed23f4a6352da
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-05-04 00:00:26 +0900

    컴포넌트 분리

--- src/App.vue
+++ src/App.vue
@@ -2,86 +2,32 @@
   <!-- App.vue -->
 
   <v-app>
-    <v-system-bar app>
-      <v-spacer></v-spacer>
+    <SystemBar/>
 
-      <v-icon>mdi-square</v-icon>
+    <NavigationDrawer/>
 
-      <v-icon>mdi-circle</v-icon>
+    <AppBar/>
 
-      <v-icon>mdi-triangle</v-icon>
-    </v-system-bar>
-
-    <v-navigation-drawer
-        v-model="drawer"
-        app
-    >
-      <v-sheet
-          color="grey lighten-4"
-          class="pa-4"
-      >
-        <v-avatar
-            class="mb-4"
-            color="grey darken-1"
-            size="64"
-        ></v-avatar>
-
-        <div>john@vuetifyjs.com</div>
-      </v-sheet>
-
-      <v-divider></v-divider>
-
-      <v-list>
-        <v-list-item
-            v-for="[icon, text] in links"
-            :key="icon"
-            link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ text }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-app-bar app>
-      <!-- -->
-    </v-app-bar>
-
-    <!-- Sizes your content based upon application components -->
     <v-main>
-
-      <!-- Provides the application the proper gutter -->
       <v-container fluid>
 
-        <!-- If using vue-router -->
         <router-view></router-view>
       </v-container>
     </v-main>
 
-    <v-footer app>
-      <!-- -->
-    </v-footer>
+    <SystemFooter/>
   </v-app>
 </template>
 
 <script>
 
+import SystemBar from '@/SystemBar';
+import NavigationDrawer from '@/NavigationDrawer';
+import AppBar from '@/AppBar';
+import SystemFooter from '@/SystemFooter';
+
 export default {
   name: 'App',
-  data: () => ({
-    cards: ['Today', 'Yesterday'],
-    drawer: null,
-    links: [
-      ['mdi-inbox-arrow-down', 'Inbox'],
-      ['mdi-send', 'Send'],
-      ['mdi-delete', 'Trash'],
-      ['mdi-alert-octagon', 'Spam'],
-    ],
-  }),
+  components: {SystemFooter, AppBar, NavigationDrawer, SystemBar},
 };
 </script>
