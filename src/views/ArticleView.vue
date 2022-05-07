91e968130aef3838d9d10eb5b483242d74a96c4e
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-05-08 02:21:30 +0900

    삭제 비활성화

--- src/views/ArticleView.vue
+++ src/views/ArticleView.vue
@@ -82,17 +82,17 @@
             </v-card-actions>
           </v-card>
         </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title>Are you sure you want to delete this item?</v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
+<!--        <v-dialog v-model="dialogDelete" max-width="500px">-->
+<!--          <v-card>-->
+<!--            <v-card-title>Are you sure you want to delete this item?</v-card-title>-->
+<!--            <v-card-actions>-->
+<!--              <v-spacer></v-spacer>-->
+<!--              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>-->
+<!--              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>-->
+<!--              <v-spacer></v-spacer>-->
+<!--            </v-card-actions>-->
+<!--          </v-card>-->
+<!--        </v-dialog>-->
       </v-toolbar>
     </template>
     <template v-slot:[`item.actions`]="{ item }">
@@ -102,12 +102,12 @@
       >
         mdi-pencil
       </v-icon>
-      <v-icon
-          small
-          @click="deleteItem(item)"
-      >
-        mdi-delete
-      </v-icon>
+<!--      <v-icon-->
+<!--          small-->
+<!--          @click="deleteItem(item)"-->
+<!--      >-->
+<!--        mdi-delete-->
+<!--      </v-icon>-->
     </template>
     <template v-slot:no-data>
       <v-btn
