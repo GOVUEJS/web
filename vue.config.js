97965937ca471cf4931dfd004884669fbb2dcc95
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-05-07 22:33:09 +0900

    cors 테스트

--- vue.config.js
+++ vue.config.js
@@ -8,13 +8,5 @@ module.exports = defineConfig({
         client: {
             overlay: false,
         },
-        proxy: {
-            // url 리소스를 추가해주자
-            "/api": {
-                target: `${process.env.VUE_APP_API_SERVER}`,
-                changeOrigin: true,
-                pathRewrite: {'^/api': ''},
-            },
-        }
     },
 });
