58460673ed260621248f0f34cff37d46c069fd25
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-05-20 02:30:33 +0900

    axios typescript 적용

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
-function setInterceptor(instance) {
+function setInterceptor(instance: AxiosInstance) {
     // 요청 인터셉터 추가하기
     axios.interceptors.request.use(function (config) {
         // 요청이 전달되기 전에 작업 수행
