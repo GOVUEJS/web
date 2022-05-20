a0a7be0a4f67b3327b3886969abc9b6c7be17bfb
Changmin Lee <50049254+Hwisaek@users.noreply.github.com>
2022-05-20 15:10:08 +0900

    vuex -> pinia

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify';
+import { loadFonts } from './plugins/webfontloader';
+import { createPinia } from 'pinia';
 
-loadFonts()
+loadFonts();
 
-createApp(App).use(createPinia())
+createApp(App)
+  .use(createPinia())
   .use(router)
-  .use(store)
   .use(vuetify)
-  .mount('#app')
+  .mount('#app');
